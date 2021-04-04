4e762b3b067541792265576592c6943cf8a1da82
Sharing is Caring <prabeshrajjk@gmail.com>
2021-04-04 20:41:25 +0545

    Update IDM TRIAL RESET_ Run once.bat

--- IDM TRIAL RESET_ Run once.bat
+++ IDM TRIAL RESET_ Run once.bat
@@ -2,8 +2,11 @@
 @echo off
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "%~dp0"
-@Echo copying the bat file to IDM Folder
-Robocopy "%~dp0/IDM Trial Reset Files/IDM_Reg_clean_PrJ.bat" "C:\Program Files (x86)\Internet Download Manager\IDM_Reg_clean_PrJ.bat"
+:: Taking access permission 
+icacls "C:\Program Files (x86)\Internet Download Manager" /q /c /t /grant Everyone:F
+:: copying the bat file to IDM Folder
+
+copy "%~dp0/IDM Trial Reset Files\IDM_Reg_clean_PrJ_test.bat" "C:\Program Files (x86)\Internet Download Manager\"
 
 @Echo  Adding the IDM trail reset bat file in task schedule to run it everytime the device is on
 
